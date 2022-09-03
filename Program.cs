567f5d01c00d819604388b7280b6a62ddccb0cd7
Alik <9774952689@bk.ru>
2022-09-04 02:24:00 +0300

    CompletedForester

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+int c = 1;
+if (a>9 || a<1)
+Console.WriteLine("Введено неверное число");
+    else
+    {
+    while (b<=a)
+        {
+        Console.WriteLine(c);
+        b=b+1;
+        c=(c*10)+b;
+    
+        }
+    }
